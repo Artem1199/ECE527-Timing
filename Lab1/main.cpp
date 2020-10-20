1cad8a7cde59598fb07c0505f7e6352cd4c30c63
Artem1199 <51388454+Artem1199@users.noreply.github.com>
2020-10-19 18:41:42 -0700

    added better comments

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -2,23 +2,34 @@
 #include <math.h>    
 using namespace std;
 
+
 #define DMAX 2
 #define N 4
+
+// number to represent infinity
+// Need to replace with a better rep. in the future
 int inf = 9999;
 
 
-//[row][column]
 int FW(int dist[N][N]);
+void retime (int graph[N][N], int c);
 
 
+////////////////////////////////////////////////////////////////////////////////////
+// Matrix: class that is just a matrix, but has a couple extra functions
+////////////////////////////////////////////////////////////////////////////////////
 class Matrix {
-    
     public:
     int a[N][N] = {0};
     void sp();
     void set_values(int ro[N][N]);
+    void print_matrix();
 };
 
+
+////////////////////////////////////////////////////////////////////////////////////
+// set_values: input a matrix and store the values into a "matrix" class
+////////////////////////////////////////////////////////////////////////////////////
 void Matrix::set_values(int ro[N][N]){
     for (int v = 0; v < N; v++){
         for (int u = 0; u < N; u++){
@@ -27,16 +38,22 @@ void Matrix::set_values(int ro[N][N]){
     }
 };
 
+////////////////////////////////////////////////////////////////////////////////////
+// sp: shortest path method uses Floyd-Warshall algorithm to find the shortest
+// path in a matrix describing a graph.
+////////////////////////////////////////////////////////////////////////////////////
 void Matrix::sp(){
-    // dp[matrix][row][column] [k][U][V]
+
     int dp[N+1][N][N] = {0};
 
+// copy the 2d matrix into a 3rd matrix
     for (int v = 0; v < N; v++){
         for (int u = 0; u < N; u++){
-            dp[0][u][v] = a[u][v];
+            dp[0][u][v] = a[u][v];  // on first instances a = G (graph rep)
         }
     }
 
+// meat of the algorithm, finds the actualy shortest path
     for (int k = 0; k < N; k++){
         for (int v = 0; v < N; v++){
             for (int u = 0; u < N; u++){
@@ -48,67 +65,81 @@ void Matrix::sp(){
                 }
             }
         }
-    }    
+    } 
+
+// return the found shortest path values back to the matrix
     for (int v = 0; v < N; v++){
         for (int u = 0; u < N; u++){
-            a[u][v] = dp[N][u][v];
+            a[u][v] = dp[N][u][v];  // shortest path values are found in the last matrix N
         }
     }
 };
 
-void retime (int graph[N][N], int c);
+void Matrix::print_matrix(){
+
+
+    return;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+// main: Defines our graph and C value, then calls the retime function
+////////////////////////////////////////////////////////////////////////////////////
 
 int main() {
 
-    int init_graph[N][N] ={ {inf, inf, 1, 2},
-                    {1, inf, inf, inf},
-                    {inf, 0, inf, inf},
-                    {inf, 0, inf, inf}
-                    };
+/* representation of the graph that we want to test */
+    int init_graph[N][N] ={ 
+                         /*1*/   {inf, inf,   1,   2},
+                         /*2*/    {  1, inf, inf, inf},
+                         /*3*/    {inf,   0, inf, inf},
+                         /*4*/    {inf,   0, inf, inf}
+                            };
+    int c = 3; // wanted retiming value
 
-    retime(init_graph, 3);
-    
-    
+    retime(init_graph, c);    
     return 0;
-
 }
 
 
+////////////////////////////////////////////////////////////////////////////////////
+// retime: Retiming function that takest he input graph, and a delay time (c)
+// prints out retimed constraints.  Runs recursively until lowest time is achieved
+////////////////////////////////////////////////////////////////////////////////////
+
 void retime (int graph[N][N], int c){
 
-
-    // a[row][column]
+/* declare matrices used in program */
     Matrix G;
     Matrix Gp;
     Matrix W;
     Matrix D;
-    
+    Matrix INQ;
 
-    G.set_values(graph);
+    int dU[N] = {1,1,2,2};  // propagation delays of each circuit 
 
-    int dU[N] = {1,1,2,2};
+    G.set_values(graph); // set matrix G to graph
+    Gp = G;
 
     for (int i = 0; i < N ; i++){
         cout << i + 1 << "  |";
         for (int j = 0; j < N; j++){
             if ((G.a[i][j] < inf) && (i != j)){
-                G.a[i][j] = N * DMAX * G.a[i][j] - dU[i];
+                Gp.a[i][j] = N * DMAX * G.a[i][j] - dU[i];
             };
-            cout << G.a[i][j] << "   "; 
+            cout << Gp.a[i][j] << "   "; 
         };
         cout << "\n";
     };
 
 /*Finding Shortest Path Suv*/
-    G.sp();
-
+    Gp.sp(); // this G becomes "Suv"
 
 /* Finding W(U,V) */
     int w[N][N] = {0};
         for (int v = 0; v < N; v++){
             for (int u = 0; u < N; u++){
                 if (u!=v){
-                        w[u][v] = ceil(((float) G.a[u][v]) / (N * DMAX));
+                        w[u][v] = ceil(((float) Gp.a[u][v]) / (N * DMAX)); // Gp reps Suv
                 }
             }
         }
@@ -120,7 +151,7 @@ void retime (int graph[N][N], int c){
     for (int v = 0; v < N; v++){
         for (int u = 0; u < N; u++){
             if (u!=v){
-                    d[u][v] = (N * DMAX) * w[u][v] - G.a[u][v] + dU[v];  // Note: using w not W.a here
+                    d[u][v] = (N * DMAX) * w[u][v] - Gp.a[u][v] + dU[v];  // Note: using w not W.a here
                } else {
                    d[u][v] = dU[u];
                }
@@ -128,6 +159,7 @@ void retime (int graph[N][N], int c){
     }    
     D.set_values(d);
 
+/* finding inequalities */
     int inq[N][N];
 
     for (int v = 0; v < N; v++){
@@ -138,25 +170,24 @@ void retime (int graph[N][N], int c){
                 inq[u][v] = w[u][v];
             }
         }
-    }   
-
-    Matrix INQ;
-
+    }
     INQ.set_values(inq);
 
+/* Reuse SP algorithm to find SP for inequalities */
     INQ.sp();
 
     int c_tmp = 0;
-    for(int i = 0; i < N; i++){
-        cout << "R" << i+1 << "(" << INQ.a[i][1] << "), ";
+        for(int i = 0; i < N; i++){
+            cout << "R" << i+1 << "(" << INQ.a[i][1] << "), ";
 
-    c_tmp += INQ.a[i][1];
+        c_tmp += INQ.a[i][1];
 
     }
 
     cout << "CYCLE IS EQUAL TO: " << c_tmp << "\n";
     if (c_tmp >= 0){
-        retime(graph, c-1);
+       // if (c >= -3){
+        retime(graph, c-1); // recursive call to retime function
     }
 
     // Not sure if this is the correct "negative cycle" setup
@@ -164,67 +195,3 @@ void retime (int graph[N][N], int c){
     return;
     
 }
-
-
-
-
-/* Formualte inequalities */
-
-
-
-
-   // FW(a);
-
-
-
-
-/*int FW(int *dist){
-    // dp[matrix][row][column] [k][U][V]
-    int dp[N+1][N][N] = {0};
-
-    for (int v = 0; v < N; v++){
-        for (int u = 0; u < N; u++){
-            dp[0][u][v] = dist[u][v];
-        }
-    }
-
-    for (int k = 0; k < N; k++){
-        for (int v = 0; v < N; v++){
-            for (int u = 0; u < N; u++){
-                dp[k+1][u][v] = dp[k][u][v];
-                    if (dp[k+1][u][v] > (dp[k][u][k]+dp[k][k][v]) 
-                        && !(dp[k][u][k]+dp[k][k][v]+dp[k+1][u][v] > 19000)){ // 2nd condition to verify that inf > inf is false, to avoid the instance where (inf > inf - 2) would be true
-                                                                                // if two of these added is greater than 19000, then we must have 2 inf, and inf > inf is false
-                        dp[k+1][u][v] = dp[k][u][k] + dp[k][k][v];
-                }
-            }
-        }
-    }
-    
-}*/
-/*
-int compute_Gprime () {
-    // used in calculation of W and D
-
-
-}
-
-int compute_W () {
-    // used to formulate ineql
-}
-
-int compute_D () {
- // used to formulate ineql
-}
-
-int compute_iql () {
-
-}
-
-int solve_iql () {
-
-}
-
-int shortest_path () {
-
-}*/
